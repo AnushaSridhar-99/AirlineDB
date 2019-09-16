854c8e0c43442ba84c64ce0dba6c0c4a6d77f129
FanGirlAnusha <48248465+FanGirlAnusha@users.noreply.github.com>
2019-09-16 21:38:50 +0530

    Add files via upload

---
+++ airline.js
@@ -0,0 +1,19 @@
+function openForm1() {
+  document.getElementById("myForm1").style.display = "block";
+}
+function openForm2() {
+  document.getElementById("myForm2").style.display = "block";
+}
+function openForm3() {
+  document.getElementById("myForm3").style.display = "block";
+}
+
+function closeForm1() {
+  document.getElementById("myForm1").style.display = "none";
+}
+function closeForm2() {
+  document.getElementById("myForm2").style.display = "none";
+}
+function closeForm3() {
+  document.getElementById("myForm3").style.display = "none";
+}
