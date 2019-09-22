7b911a46191083c9e3b4d5ae22821be207ad04c5
Anusha Sridhar <anusha.s.naidu@gmail.com>
2019-09-22 15:40:00 +0530

    file upload

--- airline.js
+++ airline.js
@@ -4,11 +4,16 @@ function openForm1() {
 function openForm2() {
   document.getElementById("myForm2").style.display = "block";
 }
-
+function openForm3() {
+  document.getElementById("myForm3").style.display = "block";
+}
 
 function closeForm1() {
   document.getElementById("myForm1").style.display = "none";
 }
 function closeForm2() {
   document.getElementById("myForm2").style.display = "none";
+}
+function closeForm3() {
+  document.getElementById("myForm3").style.display = "none";
 }
