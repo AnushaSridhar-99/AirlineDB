a2169a312bb2b2e01f154bbaced1bc73939d44f9
Anusha Sridhar <anusha.s.naidu@gmail.com>
2019-12-05 14:36:20 +0530

    Update airlines6.sql

--- airlines6.sql
+++ airlines6.sql
@@ -22,6 +22,40 @@ SET time_zone = "+00:00";
 -- Database: `airlines`
 --
 
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `BOOKING` (IN `UserName` VARCHAR(20), IN `flight` VARCHAR(20), IN `number` INT, IN `amount` FLOAT, IN `stats` VARCHAR(20), IN `payment` VARCHAR(20))  BEGIN
+insert into booking (username, flightID, no_of_tickets, price, status, Paymentmethod) values(UserName, flight, number, amount, stats, payment);
+
+update flights set Capacity = Capacity-number where flightID = flight;
+
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `AdminName` varchar(20) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `contact` int(10) NOT NULL,
+  `email` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`AdminName`, `password`, `contact`, `email`) VALUES
+('admin', 'password', 11111, 'admin@gmail.com');
+
+
 -- --------------------------------------------------------
 
 --
@@ -34,7 +68,8 @@ CREATE TABLE `booking` (
   `flightID` varchar(20) NOT NULL,
   `no_of_tickets` int(11) NOT NULL,
   `price` int(11) NOT NULL,
-  `seatnumbers` int(11) NOT NULL
+  `status` varchar(20) DEFAULT NULL,
+  `Paymentmethod` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -58,9 +93,35 @@ CREATE TABLE `flights` (
 --
 
 INSERT INTO `flights` (`flightID`, `departure`, `destination`, `Date`, `Time`, `Capacity`, `Price`) VALUES
-('aa12', 'Bangalore', 'Delhi', '2019-10-18', '15:17:20', 100, 2000),
-('aa15', 'Mumbai', 'Hyderabad', '2019-10-23', '08:15:00', 120, 1800),
-('aa18', 'Bangalore', 'Gujarat', '2019-11-29', '19:20:00', 140, 1200);
+('bd22', 'Bangalore', 'Delhi', '2019-12-08', '23:00:00', 138, 3700),
+('bg22', 'Bangalore', 'Gujarat', '2019-12-11', '02:15:00', 160, 3400),
+('bh11', 'Bangalore', 'Hyderabad', '2019-12-09', '05:10:00', 160, 3500),
+('bm11', 'Bangalore', 'Mumbai', '2019-12-12', '06:20:00', 100, 3650),
+('db11', 'Delhi', 'Bangalore', '2019-12-13', '14:00:00', 147, 4300),
+('dg11', 'Delhi', 'Gujarat', '2019-12-14', '09:10:00', 165, 3700),
+('dh11', 'Delhi', 'Hyderabad', '2019-12-13', '16:30:00', 154, 4150),
+('dm11', 'Delhi', 'Mumbai', '2019-12-19', '04:40:00', 160, 3700),
+('gb11', 'Gujarat', 'Bangalore', '2019-12-11', '03:10:00', 135, 3175),
+('gd11', 'Gujarat', 'Delhi', '2019-12-10', '00:30:00', 165, 4175),
+('gh11', 'Gujarat', 'Hyderabad', '2019-12-10', '22:30:00', 150, 4265),
+('gm11', 'Gujarat', 'Mumbai', '2019-12-12', '18:15:00', 160, 3790),
+('hb11', 'Hyderabad', 'Bangalore', '2019-12-19', '23:00:00', 140, 3200),
+('hd11', 'Hyderabad', 'Delhi', '2019-12-15', '05:20:00', 150, 3100),
+('hg11', 'Hyderabad', 'Gujarat', '2019-12-11', '06:30:00', 120, 3400),
+('hm11', 'Hyderabad', 'Mumbai', '2019-12-13', '09:00:00', 155, 3150),
+('mb11', 'Mumbai', 'Bangalore', '2019-12-14', '17:25:00', 165, 2850),
+('md11', 'Mumbai', 'Delhi', '2019-12-12', '19:45:00', 140, 2500),
+('mg11', 'Mumbai', 'Gujarat', '2019-12-13', '08:00:00', 150, 2800),
+('mh11', 'Mumbai', 'Hyderabad', '2019-12-13', '20:20:00', 150, 2900);
+
+
+--
+-- Triggers `flights`
+--
+DELIMITER $$
+CREATE TRIGGER `status` BEFORE DELETE ON `flights` FOR EACH ROW UPDATE booking SET status = "cancelled" where booking.flightID = old.flightID
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -71,33 +132,13 @@ INSERT INTO `flights` (`flightID`, `departure`, `destination`, `Date`, `Time`, `
 CREATE TABLE `passengers` (
   `PNRNumber` int(5) NOT NULL,
   `flightID` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
   `name` varchar(20) NOT NULL,
   `age` int(11) NOT NULL,
   `gender` varchar(1) NOT NULL,
   `contact` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `passengers`
---
-
-INSERT INTO `passengers` (`PNRNumber`, `flightID`, `name`, `age`, `gender`, `contact`) VALUES
-(47, 'aa12', 'abc', 20, 'F', 2434),
-(48, 'aa12', 'aaa', 23, 'm', 6546546);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transactions`
---
-
-CREATE TABLE `transactions` (
-  `transacID` varchar(20) NOT NULL,
-  `BookingID` varchar(20) NOT NULL,
-  `booking_date` date NOT NULL,
-  `amount` int(11) NOT NULL,
-  `paymentMethod` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -110,24 +151,20 @@ CREATE TABLE `users` (
   `password` varchar(20) NOT NULL,
   `Fname` varchar(20) NOT NULL,
   `Lname` varchar(20) NOT NULL,
-  `Contact` int(10) NOT NULL
+  `Contact` int(10) NOT NULL,
+  `email` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`username`, `password`, `Fname`, `Lname`, `Contact`) VALUES
-('aaa', 'bbb', 'Mickey', 'Mouse', 887868),
-('abc', '133', 'anusha', 'sridhar', 2147483647),
-('anusha', '123', 'anusha', 'sridhar', 2147483647),
-('bbb', 'aaa', 'abc', 'xyz', 12345),
-('ccc', 'ggfgh', 'bfhg', 'nbjh', 453432),
-('xyz', 'hgh', 'Mickey', 'Mouse', 12343);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`AdminName`);
 
 --
 -- Indexes for table `booking`
@@ -150,13 +187,7 @@ ALTER TABLE `passengers`
   ADD PRIMARY KEY (`PNRNumber`),
   ADD UNIQUE KEY `PNRNumber` (`PNRNumber`),
   ADD KEY `flightID` (`flightID`);
-
---
--- Indexes for table `transactions`
---
-ALTER TABLE `transactions`
-  ADD PRIMARY KEY (`transacID`),
-  ADD KEY `BookingID` (`BookingID`);
+  ADD KEY `username` (`username`),
 
 --
 -- Indexes for table `users`
@@ -169,33 +200,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `BookingID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
+
+--
 -- AUTO_INCREMENT for table `passengers`
 --
 ALTER TABLE `passengers`
   MODIFY `PNRNumber` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `booking`
---
-ALTER TABLE `booking`
-  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`flightID`) REFERENCES `flights` (`flightID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `passengers`
---
-ALTER TABLE `passengers`
-  ADD CONSTRAINT `passengers_ibfk_1` FOREIGN KEY (`flightID`) REFERENCES `flights` (`flightID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `transactions`
---
-ALTER TABLE `transactions`
-  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`BookingID`) REFERENCES `booking` (`BookingID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
